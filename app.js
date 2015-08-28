9ed13ffba1c03b2c59210d4e611c1eccb9eeeccc
ZouSir <zou.peng.cn@gmail.com>
2015-08-28 16:54:51 +0800

    add mysql

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var specialist = require('./routes/specialist');
 var hbs = require('hbs');
 
 var app = express();
@@ -24,7 +24,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/page', routes);
+app.use('/specialist', specialist);
 
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,53 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+
+var pool = mysql.createPool({
+	connectionLimit: 10,
+	host: '10.66.105.135',
+	user: 'root',
+	password: 'passwd@db',
+	database: '9zhaowo'
+});
+
+
+function getUserInfo(id, call){
+	    pool.getConnection(function(errc, conn){
+		if(errc){
+			return res.render('error', {msg: errc});
+		}
+		
+		conn.query('SELECT wx_openid,name,wx_city,wx_country,wx_headimgurl,indeustry,mobi FROM t_corevisit', function(errx, rows, fields){
+			if(errx){
+				res.end(JSON.stringify({err:1, msg:errx}));
+				conn.release();
+				return;
+			}
+			
+			var ifaceArr = [];
+			for(var i in rows){
+				var arr = [];
+				arr.push(rows[i].Fcommand_id);
+				arr.push(rows[i].Fproduct_id);
+				arr.push(rows[i].Fscene_id);
+				arr.push(rows[i].Fspid);
+				arr.push(rows[i].Furl);
+				arr.push(0);
+				
+				ifaceArr.push(arr);
+			}
+			
+			res.end(JSON.stringify({err:0, iface:ifaceArr, container:containerListUrl}));
+			conn.release();
+		});
+	});
+}
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     var mainPage = {};
-    mainPage.user = {name: '邹鹏', wx: 'cnzoupeng', mobile: '17712345678', 
+    mainPage.user = {name: '邹鹏', wx: 'cnzoupeng', id:'123456', mobile: '17712345678', 
       area: '广东/深圳', avatar:'http://127.0.0.1/avatar/head.jpg', tag: "IT 安全 编程", intro: "全栈工程师、网络、安全、Linux、高可靠、高并发、分布式"};
       
     mainPage.msg = {count: 10, item: [{mfrom: 'songan', time:'2015-8-27 18:05:30', content:'hello'},
@@ -12,6 +55,9 @@ router.get('/', function(req, res, next) {
       
     mainPage.listUser = [{name: '宋安', hangye: '营销', area: '广东/深圳', tag: '出书 营销', intro: 'Nice', avatar:'http://9zhaowo.com/avatar/lkjsdflhljsd.jpg'},
       {name: '宋安2', hangye: '营销', area: '广东/深圳', tag: '出书 营销', intro: 'Nice', avatar:'http://9zhaowo.com/avatar/lkjsdflhljsd.jpg'},
+      {name: '宋安3', hangye: '营销', area: '广东/深圳', tag: '出书 营销', intro: 'Nice ccv', avatar:'http://9zhaowo.com/avatar/lkjsdflhljsd.jpg'},
+      {name: '宋安3', hangye: '营销', area: '广东/深圳', tag: '出书 营销', intro: 'Nice ccv', avatar:'http://9zhaowo.com/avatar/lkjsdflhljsd.jpg'},
+      {name: '宋安3', hangye: '营销', area: '广东/深圳', tag: '出书 营销', intro: 'Nice ccv', avatar:'http://9zhaowo.com/avatar/lkjsdflhljsd.jpg'},
       {name: '宋安3', hangye: '营销', area: '广东/深圳', tag: '出书 营销', intro: 'Nice ccv', avatar:'http://9zhaowo.com/avatar/lkjsdflhljsd.jpg'}];
       
     res.render('index', mainPage);
